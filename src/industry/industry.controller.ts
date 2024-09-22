db1b251f8badfab3d48ec9983dcf4185c5f24a6f
Margo-Katsan <3margo10@gmail.com>
2024-09-22 11:42:03 +0200

    Add industry service and controller

--- src/industry/industry.controller.ts
+++ src/industry/industry.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Industry } from 'src/schemas/industry.schema';
+
 import { IndustryService } from './industry.service';
 
-@Controller('industry')
+@Controller('industries')
 export class IndustryController {
   constructor(private readonly industryService: IndustryService) {}
+
+  @Get()
+  async getAllIndustries(): Promise<Industry[]> {
+    return this.industryService.findAll();
+  }
 }
--- src/industry/industry.service.ts
+++ src/industry/industry.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { MultiLanguageString } from 'src/common/types/language.types';
+import { Industry } from 'src/schemas/industry.schema';
 
 @Injectable()
-export class IndustryService {}
+export class IndustryService {
+  constructor(
+    @InjectModel(Industry.name)
+    private industryModel: Model<Industry>
+  ) {}
+
+  async findAll(): Promise<Industry[]> {
+    return this.industryModel.find().exec();
+  }
+
+  async findOrCreate(name: MultiLanguageString): Promise<Industry> {
+    let industry = await this.industryModel.findOne({ name }).exec();
+
+    if (!industry) {
+      industry = new this.industryModel({ name });
+      return industry.save();
+    }
+
+    return industry;
+  }
+}
