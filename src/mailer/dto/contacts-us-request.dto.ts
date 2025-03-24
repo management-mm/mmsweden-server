53f4f9e9cb2bceca9cf5fa3635ec0df7eca0edd0
Margo-Katsan <3margo10@gmail.com>
2025-03-24 20:28:22 +0100

    replace @IsPhoneNumber with @IsString

--- src/mailer/dto/contacts-us-request.dto.ts
+++ src/mailer/dto/contacts-us-request.dto.ts
@@ -10,7 +10,7 @@ export class ContactUsRequestDto {
   email: string;
 
   @IsNotEmpty()
-  @IsPhoneNumber()
+  @IsString()
   phone: string;
 
   @IsNotEmpty()
--- src/mailer/dto/sell-to-us-request.dto.ts
+++ src/mailer/dto/sell-to-us-request.dto.ts
@@ -10,7 +10,7 @@ export class SellToUsRequestDto {
   email: string;
 
   @IsNotEmpty()
-  @IsPhoneNumber()
+  @IsString()
   phone: string;
 
   @IsNotEmpty()
