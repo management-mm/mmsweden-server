c8df2f0a472de47e97fc7bf27fcf8bbf5141c62e
Margo-Katsan <3margo10@gmail.com>
2026-03-16 15:02:19 +0100

    feat(counter): add endpoint to get current product counter

--- src/counters/counters.module.ts
+++ src/counters/counters.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
-import { Counter, CounterSchema } from './counters.schema';
+import { Counter, CounterSchema } from '../schemas/counters.schema';
 import { CountersService } from './counters.service';
+import { CountersController } from './counters.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Counter.name, schema: CounterSchema }]),
   ],
+  controllers: [CountersController],
   providers: [CountersService],
   exports: [CountersService],
 })
